114120044005aec2ac24c004f2a422aaa2a963ac
Heiko Braun <ike.braun@googlemail.com>
2017-04-03 12:32:02 +0200

    OBST-179: Align with Exp-104 contents

---
+++ src/test/java/org/obsidiantoaster/quickstart/health/GreetingServiceTest.java
@@ -0,0 +1,62 @@
+package org.obsidiantoaster.quickstart.health;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.wildfly.swarm.arquillian.DefaultDeployment;
+
+/**
+ * @author Heiko Braun
+ */
+@RunWith(Arquillian.class)
+@DefaultDeployment
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class GreetingServiceTest {
+
+    @Test
+    @RunAsClient
+    public void test_A_service_invocation() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080")
+                .path("api").path("greeting");
+
+        Response response = target.request(MediaType.APPLICATION_JSON).get();
+        Assert.assertEquals(200, response.getStatus());
+        Assert.assertTrue(response.readEntity(String.class).contains("Hello, World!"));
+    }
+
+    @Test
+    @RunAsClient
+    public void test_B_service_killed() throws Exception {
+        Client client = ClientBuilder.newClient();
+        WebTarget killme = client.target("http://localhost:8080")
+                .path("api").path("killme");
+
+        // suspend process
+        Response response = killme.request().get();
+        Assert.assertEquals(200, response.getStatus());
+
+        client.close();
+        Thread.sleep(150);
+
+        client = ClientBuilder.newClient(); // new connection
+
+        // verify that it yields 503
+        WebTarget greeting = client.target("http://localhost:8080")
+                       .path("api").path("greeting");
+
+
+        response = greeting.request().get();
+        Assert.assertEquals(503, response.getStatus());
+    }
+}
---
+++ src/test/java/org/obsidiantoaster/quickstart/health/OpenshiftIT.java
@@ -0,0 +1,75 @@
+package org.obsidiantoaster.quickstart.health;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import static io.restassured.RestAssured.expect;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+
+/**
+ * @author Heiko Braun
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class OpenshiftIT {
+
+    private static final String APPLICATION_NAME = System.getProperty("app.name");
+
+    private static final OpenshiftTestAssistant openshift = new OpenshiftTestAssistant(APPLICATION_NAME);
+
+    @BeforeClass
+    public static void setup() throws Exception {
+
+        Assert.assertNotNull(APPLICATION_NAME);
+
+        // the application itself
+        openshift.deployApplication();
+
+        // wait until the pods & routes become available
+        openshift.awaitApplicationReadinessOrFail();
+
+    }
+
+    @AfterClass
+    public static void teardown() throws Exception {
+       openshift.cleanup();
+    }
+
+    @Test
+    public void test_A_ServiceInvocation() {
+
+        expect().
+                statusCode(200).
+                body(containsString("Hello, World!")).
+        when().
+            get(openshift.getBaseUrl() + "/api/greeting");
+    }
+
+    @Test
+    public void test_B_ServiceKilled() throws Exception {
+
+        // suspend service
+        expect().
+                statusCode(200).
+        when().
+                get(openshift.getBaseUrl() + "/api/killme");
+
+        Thread.sleep(500);
+
+        // verify it has been suspended
+        expect().
+                statusCode(503).
+        when().
+                get(openshift.getBaseUrl() + "/api/greeting");
+    }
+}
+
